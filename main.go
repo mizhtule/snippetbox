4059c6e989820094bcbd90db807c27c97cb272cf
Lê Minh Tú <mizhtule@gmail.com>
2025-09-18 17:04:52 +0700

    Web application basics

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-    fmt.Println("Hello, World!")
+	// Use the http.NewServeMux() function to initialize a new serve mux, then
+	// register the home function as the handler for the "/" URL pattern.
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", home)
+
+	// Use the http.ListenAndServe() function to start a new web server. We pass in
+	// two parameters: the TCP network address to listen on (in this case ":4000")
+	// and the serve mux we just created. If http.ListenAndServe() returns an error
+	// we use the log.Fatal() function to log the error message and exit.
+	// Note that any error returned by http.ListenAndServe() is always non-nil.
+	log.Print("Starting server on :4000")
+	err := http.ListenAndServe(":4000", mux)
+	log.Fatal(err)
+}
+
+func home(w http.ResponseWriter, _ *http.Request) {
+	w.Write([]byte("Hello from Snippetbox!"))
 }
